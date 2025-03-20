d7490d118a625e945b55e5b0576d72209de95802
Carlos Torres <138171454+cawtoz@users.noreply.github.com>
2025-03-20 17:56:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cawtoz.github.io',
-    base: '/portfolio'
+    site: 'https://cawtoz.dev',
+    base: '/'
 });
